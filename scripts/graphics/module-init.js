5bb5b36602141d365011f105eb58194fdfedafb5
Norberttony <norberttony@icloud.com>
2025-02-06 09:17:43 -0500

    test-path

--- scripts/graphics/module-init.js
+++ scripts/graphics/module-init.js
@@ -7,7 +7,7 @@ const module_loader = new Module_Loader();
     const gameScripts = [ "coords", "game", "move", "piece", "pre-game", "san" ];
 
     for (const scriptName of gameScripts){
-        module_loader.load(`/scripts/game/${scriptName}.mjs`).then(globalize);
+        module_loader.load(`./scripts/game/${scriptName}.mjs`).then(globalize);
     }
 
     await module_loader.waitForAll();
